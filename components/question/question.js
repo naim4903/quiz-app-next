87be9002af3b057401414d709a202033d117fc91
Naim Tarafder <naim4903@gmail.com>
2022-10-30 18:58:55 +0530

    question component

---
+++ components/question/question.js
@@ -0,0 +1,54 @@
+import { Button, Grid } from "@mui/material"
+import { useState, useEffect } from "react"
+
+const Question = ({ questions, correctAns, setScore, score }) => {
+    const [selected, setSelected] = useState(null);
+
+    useEffect(() => {
+        setSelected(null);
+    }, [correctAns])
+
+    const checkAnsewar = (ans) => {
+        if (selected) {
+            return
+        } else {
+            setSelected(ans);
+            if (ans === correctAns) setScore(score + 1)
+        }
+    };
+
+    const handleSelect = (ans) => {
+        if (selected === ans && correctAns === selected) {
+            return "correct-answear";
+        } else if (selected === ans && correctAns !== selected) {
+            return "wrong-answear";
+        } else if (ans === correctAns) {
+            return "correct-answear";
+        } else if (ans !== correctAns) {
+            return "wrong-answear";
+        }
+    }
+
+    return (
+        <>
+            {
+                questions && questions.map((item, i) => (
+                    <Grid item xs={12} md={6} key={i}>
+                        <Button
+                            variant="outlined"
+                            color="info"
+                            fullWidth
+                            disabled={selected}
+                            className={`option ${selected && handleSelect(item)}`}
+                            onClick={(e) => checkAnsewar(item)}
+                        >
+                            {item}
+                        </Button>
+                    </Grid>
+                ))
+            }
+        </>
+    )
+}
+
+export default Question
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react"
 import { useRouter } from "next/router"
 import axios from "axios"
 import { Button, Card, CardContent, CardHeader, Grid } from "@mui/material"
+import Question from "../../components/question/question"
 
 
 const Quiz = () => {
     const [questions, setQuestions] = useState([]);
-    const [answear, setAnswear] = useState(0);
+    const [data, setData] = useState([]);
+    const [score, setScore] = useState(0);
     const [questionNumber, setQuestionNumber] = useState(0);
     const [name, setName] = useState("");
     const [complete, setComplete] = useState(false);
@@ -25,7 +27,7 @@ const Quiz = () => {
             let resp = await axios.get(`https://opentdb.com/api.php?amount=10&category=${categoryId}&difficulty=${difficulty}&type=multiple`)
             if (resp.data.results.length > 0) {
                 let tempData = [];
-
+                setData(resp.data.results);
                 resp.data.results.forEach((item) => {
                     tempData.push({
                         answear: [item.correct_answer, item.incorrect_answers[0], item.incorrect_answers[1], item.incorrect_answers[2]],
@@ -79,75 +81,53 @@ const Quiz = () => {
         <Layout name={name}>
             {
                 complete ?
-                    <>
-                        <Card style={{ textAlign: "center", padding: "2rem 3rem 1rem" }}>
-                            <CardContent>
-                                <p style={{ fontSize: "1.2rem", textTransform: "capitalize" }}>Congratulations! {name}</p>
-                                <p style={{ fontSize: "3rem" }}>Your Score {answear}</p>
-                                <Button variant="contained" color="success" size="large" onClick={() => router.replace("/")}>Go To Home</Button>
-                            </CardContent>
-                        </Card>
-                    </> :
-                    <>
-                        {questions.map((item, i) => (
-                            i === questionNumber ?
-                                <Grid key={i}>
-                                    <Grid item xs={12} >
-                                        <Card style={{ textAlign: "center", padding: "2rem .6rem 1rem" }}>
-                                            <span>{item.category}</span>
-                                            {/* <CardHeader title={i + 1 + ". " + item.question} subheader={`${answear}/10`} /> */}
-                                            <h4 style={{ padding: 4, margin: 0 }}>{i + 1 + ". " + item.question}</h4>
-                                            <p style={{}}>{answear}/10</p>
-                                            <CardContent>
-                                                <Grid container spacing={2}>
-                                                    <Grid item xs={12} md={6}>
-                                                        <Button variant="outlined" color="info" fullWidth className="option" onClick={(e) => checkAnsewar(e, item.answear[0], item.correctAnswear)}>
-                                                            a. {item.answear[0]}
-                                                        </Button>
-                                                    </Grid>
-                                                    <Grid item xs={12} md={6}>
-                                                        <Button variant="outlined" color="info" fullWidth className="option" onClick={(e) => checkAnsewar(e, item.answear[1], item.correctAnswear)}>
-                                                            b. {item.answear[1]}
-                                                        </Button>
-                                                    </Grid>
-                                                    <Grid item xs={12} md={6}>
-                                                        <Button variant="outlined" color="info" fullWidth className="option" onClick={(e) => checkAnsewar(e, item.answear[2], item.correctAnswear)}>
-                                                            c. {item.answear[2]}
-                                                        </Button>
-                                                    </Grid>
-                                                    <Grid item xs={12} md={6}>
-                                                        <Button variant="outlined" color="info" fullWidth className="option" onClick={(e) => checkAnsewar(e, item.answear[3], item.correctAnswear)}>
-                                                            d. {item.answear[3]}
-                                                        </Button>
-                                                    </Grid>
-                                                    <Grid item xs={1} md={4}></Grid>
-                                                    <Grid item xs={5} md={2}>
-                                                        <Button
-                                                            variant="contained"
-                                                            size="small"
-                                                            color="error"
-                                                            fullWidth
-                                                            onClick={onQuitClick}
-                                                        >quit</Button>
-                                                    </Grid>
-                                                    <Grid item xs={5} md={2}>
-                                                        <Button
-                                                            variant="contained"
-                                                            size="small"
-                                                            color="warning"
-                                                            fullWidth
-                                                            onClick={onNextClick}
-                                                        >Next</Button>
-                                                    </Grid>
-                                                </Grid>
-                                            </CardContent>
-                                        </Card>
+                    <Card style={{ textAlign: "center", padding: "2rem 3rem 1rem" }}>
+                        <CardContent>
+                            <p style={{ fontSize: "1.2rem", textTransform: "capitalize" }}>Congratulations! {name}</p>
+                            <p style={{ fontSize: "3rem" }}>Your Score {score}</p>
+                            <Button variant="contained" color="success" size="large" onClick={() => router.replace("/")}>Go To Home</Button>
+                        </CardContent>
+                    </Card>
+                    :
+                    <Grid>
+                        <Grid item xs={12} >
+                            <Card style={{ textAlign: "center", padding: "2rem .6rem 1rem" }}>
+                                <span>{data[questionNumber]?.category}</span>
+                                {/* <CardHeader title={1 + ". " + data[questionNumber]?.question} subheader={`${answear}/10`} /> */}
+                                <h4 style={{ padding: 4, margin: 0 }}>{1 + ". " + data[questionNumber]?.question}</h4>
+                                <p style={{}}>{score}/10</p>
+                                <CardContent>
+                                    <Grid container spacing={2}>
+                                        <Question
+                                            questions={questions[questionNumber]?.answear}
+                                            correctAns={data[questionNumber]?.correct_answer}
+                                            setScore={setScore}
+                                            score={score}
+                                        />
+                                        <Grid item xs={1} md={4}></Grid>
+                                        <Grid item xs={5} md={2}>
+                                            <Button
+                                                variant="contained"
+                                                size="small"
+                                                color="error"
+                                                fullWidth
+                                                onClick={onQuitClick}
+                                            >quit</Button>
+                                        </Grid>
+                                        <Grid item xs={5} md={2}>
+                                            <Button
+                                                variant="contained"
+                                                size="small"
+                                                color="warning"
+                                                fullWidth
+                                                onClick={onNextClick}
+                                            >Next</Button>
+                                        </Grid>
                                     </Grid>
-                                </Grid>
-                                :
-                                null
-                        ))}
-                    </>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    </Grid>
             }
         </Layout>
     )
